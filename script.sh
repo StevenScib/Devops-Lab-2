03508bb191afd2aa5f595d11cf5798b194bb3f5a
StevenScib <x23174668@student.ncirl.ie>
2024-02-03 17:53:21 +0000

    Deltes folders but recreates so step 10 and 11 can be done

--- script.sh
+++ script.sh
@@ -3,3 +3,9 @@ mkdir fol_1
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 mkdir fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+rm -rf fol_1 fol_2
+mkdir fol_1
+mkdir fol_2
+chmod 600 fol_1/*_1.txt fol_1/*_3.txt
+chmod 600 fol_2/*_1.txt fol_2/*_3.txt
+chmod 700 fol_1/*_2.txt fol_2/*_2.txt
